a880fc44c5bd4c4ca11ad6974d7e7830fc36e816
GAPParreira <gabri.1996@hotmail.com>
2020-04-25 21:00:05 -0300

    updateCode

---
+++ ProjetoInterFrom/ProjetoInterFrom/Login.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoInterFront
+{
+    public partial class Login : Form
+    {
+        int X = 0;
+        int Y = 0;
+
+        string user = "admin";
+        string password = "admin";
+
+        public Login()
+        {
+            InitializeComponent();
+            this.MouseDown += new MouseEventHandler(Form3_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form3_MouseMove);
+        }
+
+        private void Form3_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            X = this.Left - MousePosition.X;
+            Y = this.Top - MousePosition.Y;
+        }
+
+        private void Form3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+            this.Left = X + MousePosition.X;
+            this.Top = Y + MousePosition.Y;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+
+
+        private void statusUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            statusUser.Text = "";
+            statusPass.Text = "";
+            if (textBoxUser.Text == user && textBoxPassword.Text == password)
+            {
+                MessageBox.Show("Parabens");
+            }
+            if (textBoxUser.Text != user)
+            {
+                statusUser.Text = "Usuario não existe!";
+            }
+            else
+            {
+                statusUser.Text = "";
+                if (textBoxUser.Text == user)
+                {
+                    if (textBoxPassword.Text != password)
+                    {
+                        statusPass.Text = "Senha esta incorreta!";
+                    }
+                }
+            }
+        }
+
+
+
+        private void Registrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonEmail_Click(object sender, EventArgs e)
+        {
+            String email = "acsupp@gmail.com";
+            MessageBox.Show(email);
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
